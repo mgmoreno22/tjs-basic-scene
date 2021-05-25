9d768a02cbe80ab4e519aee18e1cfcd8ebcc9547
Michael Moreno <mmoreno2222@gmail.com>
2021-05-25 16:43:03 -0700

    cube is rendered onto scene

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const scene = new THREE.Scene();
 
 // Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "0xff0000" });
+const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
 // You can call this "cube" if you're making more than one object
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+// args are Field of View, Aspect Ratio
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer (result drawn into canvas)
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
