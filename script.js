ad42a57d2a622341dba30765d59fea2d0f5f3f84
Michael Moreno <mmoreno2222@gmail.com>
2021-05-25 15:37:41 -0700

    added basic shape

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(THREE);
+// New scene from THREE object
+const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "0xff0000" });
+// You can call this "cube" if you're making more than one object
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
